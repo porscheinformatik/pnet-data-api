632ca6a99d335e33c78ace7172e53f780b669f78
Carolin Schuntermann <carolin.schuntermann@porscheinformatik.com>
2023-02-15 09:45:37 +0100

    Add sorting options for PersonDataFind
    Created an enum that can be extended with any fields desired.
    
    Updated the Orderable interface so that it can take multiple options.
    
    (cherry picked from commit cb79f8c42179beacac653e5708536c410cd66ead)

---
+++ pnet-data-api-java/src/main/java/pnet/data/api/person/PersonOrderBy.java
@@ -0,0 +1,18 @@
+package pnet.data.api.person;
+
+/**
+ * Used for sorting persons on request. <br>
+ * Extend with further options as needed.
+ * 
+ * @author scar
+ */
+public enum PersonOrderBy
+{
+    RELEVANCE,
+
+    ID,
+
+    FIRST_NAME,
+
+    LAST_NAME
+}
