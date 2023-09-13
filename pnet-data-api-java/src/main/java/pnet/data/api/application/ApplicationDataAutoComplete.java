3233b2ce949f96e1e402dd2cf89f9e4be6453281
Manfred Hantschel <manfred.hantschel@porscheinformatik.com>
2023-09-13 15:14:11 +0200

    Remove includeInactive from application
    Applications do not distiguish between active and inactive ones.

--- pnet-data-api-java/src/main/java/pnet/data/api/application/ApplicationDataAutoComplete.java
+++ pnet-data-api-java/src/main/java/pnet/data/api/application/ApplicationDataAutoComplete.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import pnet.data.api.util.AbstractAutoComplete;
 import pnet.data.api.util.AutoCompleteFunction;
-import pnet.data.api.util.IncludeInactive;
 import pnet.data.api.util.Pair;
 
 /**
@@ -14,7 +13,6 @@ import pnet.data.api.util.Pair;
  */
 public class ApplicationDataAutoComplete
     extends AbstractAutoComplete<ApplicationAutoCompleteDTO, ApplicationDataAutoComplete>
-    implements IncludeInactive<ApplicationDataAutoComplete>
 {
     public ApplicationDataAutoComplete(AutoCompleteFunction<ApplicationAutoCompleteDTO> autoCompleteFunction,
         List<Pair<String, Object>> restrictItems)
--- pnet-data-api-java/src/main/java/pnet/data/api/application/ApplicationDataSearch.java
+++ pnet-data-api-java/src/main/java/pnet/data/api/application/ApplicationDataSearch.java
@@ -3,7 +3,6 @@ package pnet.data.api.application;
 import java.util.List;
 
 import pnet.data.api.util.AbstractSearch;
-import pnet.data.api.util.IncludeInactive;
 import pnet.data.api.util.Pair;
 import pnet.data.api.util.SearchFunction;
 
@@ -11,7 +10,6 @@ import pnet.data.api.util.SearchFunction;
  * @author cet
  */
 public class ApplicationDataSearch extends AbstractSearch<ApplicationItemDTO, ApplicationDataSearch>
-    implements IncludeInactive<ApplicationDataSearch>
 {
     public ApplicationDataSearch(SearchFunction<ApplicationItemDTO> searchFunction,
         List<Pair<String, Object>> restricts)
