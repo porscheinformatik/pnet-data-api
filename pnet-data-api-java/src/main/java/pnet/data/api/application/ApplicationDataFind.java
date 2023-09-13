fb9de274bb3e8d0b3277e85073066eaeec6d3995
Manfred Hantschel <manfred.hantschel@porscheinformatik.com>
2023-09-13 15:14:11 +0200

    Remove includeInactive from application
    Applications do not distiguish between active and inactive ones.
    
    (cherry picked from commit 3233b2ce949f96e1e402dd2cf89f9e4be6453281)

--- pnet-data-api-java/src/main/java/pnet/data/api/application/ApplicationDataFind.java
+++ pnet-data-api-java/src/main/java/pnet/data/api/application/ApplicationDataFind.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import pnet.data.api.util.AbstractScrollableFind;
 import pnet.data.api.util.FindFunction;
-import pnet.data.api.util.IncludeInactive;
 import pnet.data.api.util.Pair;
 import pnet.data.api.util.RestrictMatchcode;
 import pnet.data.api.util.RestrictUpdatedAfter;
@@ -13,9 +12,8 @@ import pnet.data.api.util.Scrollable;
 /**
  * Find for applications
  */
-public class ApplicationDataFind extends AbstractScrollableFind<ApplicationItemDTO, ApplicationDataFind>
-    implements RestrictMatchcode<ApplicationDataFind>, RestrictUpdatedAfter<ApplicationDataFind>,
-    IncludeInactive<ApplicationDataFind>, Scrollable<ApplicationDataFind>
+public class ApplicationDataFind extends AbstractScrollableFind<ApplicationItemDTO, ApplicationDataFind> implements
+    RestrictMatchcode<ApplicationDataFind>, RestrictUpdatedAfter<ApplicationDataFind>, Scrollable<ApplicationDataFind>
 {
     public ApplicationDataFind(FindFunction<ApplicationItemDTO> searchFunction, List<Pair<String, Object>> restricts)
     {
--- pnet-data-api-java/src/main/java/pnet/data/api/application/ApplicationDataGet.java
+++ pnet-data-api-java/src/main/java/pnet/data/api/application/ApplicationDataGet.java
@@ -5,14 +5,13 @@ import java.util.List;
 import pnet.data.api.util.AbstractGet;
 import pnet.data.api.util.ByMatchcode;
 import pnet.data.api.util.GetFunction;
-import pnet.data.api.util.IncludeInactive;
 import pnet.data.api.util.Pair;
 
 /**
  * @author cet
  */
 public class ApplicationDataGet extends AbstractGet<ApplicationDataDTO, ApplicationDataGet>
-    implements IncludeInactive<ApplicationDataGet>, ByMatchcode<ApplicationDataDTO, ApplicationDataGet>
+    implements ByMatchcode<ApplicationDataDTO, ApplicationDataGet>
 {
     public ApplicationDataGet(GetFunction<ApplicationDataDTO> getFunction, List<Pair<String, Object>> restricts)
     {
