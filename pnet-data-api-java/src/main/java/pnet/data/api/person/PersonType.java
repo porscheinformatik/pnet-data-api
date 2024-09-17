867880ba692d0e0e6def24353c13cb7240b0dc6d
David Schöninger <david.schoeninger.ext@porscheinformatik.com>
2024-09-17 16:46:58 +0200

    Add isVirtual flag to PersonType
    PNET-5792

--- pnet-data-api-java/src/main/java/pnet/data/api/person/PersonType.java
+++ pnet-data-api-java/src/main/java/pnet/data/api/person/PersonType.java
@@ -1,19 +1,45 @@
 package pnet.data.api.person;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public enum PersonType
 {
     /**
      * A regular person, physically existing.
      */
-    PERSON,
+    PERSON(false),
 
     /**
      * A user for automated tasks, e.g. for automatically executed tests.
      */
-    BOT,
+    BOT(true),
 
     /**
      * A user for manual tests.
      */
-    TEST_USER
+    TEST_USER(true);
+
+    private final boolean isVirtual;
+
+    PersonType(boolean isVirtual)
+    {
+        this.isVirtual = isVirtual;
+    }
+
+    public boolean isVirtual()
+    {
+        return isVirtual;
+    }
+
+    public static Set<PersonType> getVirtualTypes()
+    {
+        return Arrays.stream(values()).filter(PersonType::isVirtual).collect(Collectors.toSet());
+    }
+
+    public static Set<PersonType> getNonVirtualTypes()
+    {
+        return Arrays.stream(values()).filter(type -> !type.isVirtual()).collect(Collectors.toSet());
+    }
 }
