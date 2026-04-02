9c98406868369f42f028b33d491308be86a3135a
Manfred Hantschel <manfred.hantschel@porscheinformatik.com>
2026-04-02 14:24:37 +0200

    Fix missing config
    (cherry picked from commit d0b25c55621f4e7e590bfc6c9caf200dac12ea73)

---
+++ pnet-data-api-java/src/main/java/pnet/data/api/resource/ResourcePnetDataApiClientConfig.java
@@ -0,0 +1,10 @@
+package pnet.data.api.resource;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses = { ResourcePnetDataApiClientConfig.class })
+public class ResourcePnetDataApiClientConfig {
+    // intentionally left blank
+}
