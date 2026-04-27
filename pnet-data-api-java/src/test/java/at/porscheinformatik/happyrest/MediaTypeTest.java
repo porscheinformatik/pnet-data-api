eb5db9cf76bcdaceedf7a4218d9edde15f7fdebd
Manfred Hantschel <manfred.hantschel@porscheinformatik.com>
2026-04-27 12:38:40 +0200

    Fixed an issue with XML media types

--- pnet-data-api-java/src/test/java/at/porscheinformatik/happyrest/MediaTypeTest.java
+++ pnet-data-api-java/src/test/java/at/porscheinformatik/happyrest/MediaTypeTest.java
@@ -1,66 +1,104 @@
 package at.porscheinformatik.happyrest;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.*;
-
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 class MediaTypeTest {
 
     @Test
     void testToString() {
-        assertThat(MediaType.ANY.toString(), is("*/*"));
+        MatcherAssert.assertThat(MediaType.ANY.toString(), Matchers.is("*/*"));
 
-        assertThat(MediaType.APPLICATION.toString(), is("application/*"));
-        assertThat(MediaType.APPLICATION_ATOM_XML.toString(), is("application/atom+xml"));
-        assertThat(MediaType.APPLICATION_FORM.toString(), is("application/x-www-form-urlencoded"));
-        assertThat(MediaType.APPLICATION_JSON.toString(), is("application/json"));
-        assertThat(MediaType.APPLICATION_JSON_UTF8.toString(), is("application/json;charset=UTF-8"));
-        assertThat(MediaType.APPLICATION_ANY_JSON.toString(), is("application/*+json"));
-        assertThat(MediaType.APPLICATION_OCTET_STREAM.toString(), is("application/octet-stream"));
-        assertThat(MediaType.APPLICATION_PDF.toString(), is("application/pdf"));
-        assertThat(MediaType.APPLICATION_RSS_XML.toString(), is("application/rss+xml"));
-        assertThat(MediaType.APPLICATION_XHTML_XML.toString(), is("application/xhtml+xml"));
-        assertThat(MediaType.APPLICATION_ANY_XML.toString(), is("application/*+json"));
-        assertThat(MediaType.APPLICATION_XML.toString(), is("application/xml"));
+        MatcherAssert.assertThat(MediaType.APPLICATION.toString(), Matchers.is("application/*"));
+        MatcherAssert.assertThat(MediaType.APPLICATION_ATOM_XML.toString(), Matchers.is("application/atom+xml"));
+        MatcherAssert.assertThat(
+            MediaType.APPLICATION_FORM.toString(),
+            Matchers.is("application/x-www-form-urlencoded")
+        );
+        MatcherAssert.assertThat(MediaType.APPLICATION_JSON.toString(), Matchers.is("application/json"));
+        MatcherAssert.assertThat(
+            MediaType.APPLICATION_JSON_UTF8.toString(),
+            Matchers.is("application/json;charset=UTF-8")
+        );
+        MatcherAssert.assertThat(MediaType.APPLICATION_ANY_JSON.toString(), Matchers.is("application/*+json"));
+        MatcherAssert.assertThat(
+            MediaType.APPLICATION_OCTET_STREAM.toString(),
+            Matchers.is("application/octet-stream")
+        );
+        MatcherAssert.assertThat(MediaType.APPLICATION_PDF.toString(), Matchers.is("application/pdf"));
+        MatcherAssert.assertThat(MediaType.APPLICATION_RSS_XML.toString(), Matchers.is("application/rss+xml"));
+        MatcherAssert.assertThat(MediaType.APPLICATION_XHTML_XML.toString(), Matchers.is("application/xhtml+xml"));
+        MatcherAssert.assertThat(MediaType.APPLICATION_ANY_XML.toString(), Matchers.is("application/*+xml"));
+        MatcherAssert.assertThat(MediaType.APPLICATION_XML.toString(), Matchers.is("application/xml"));
 
-        assertThat(MediaType.IMAGE.toString(), is("image/*"));
-        assertThat(MediaType.IMAGE_GIF.toString(), is("image/gif"));
-        assertThat(MediaType.IMAGE_JPEG.toString(), is("image/jpeg"));
-        assertThat(MediaType.IMAGE_PNG.toString(), is("image/png"));
+        MatcherAssert.assertThat(MediaType.IMAGE.toString(), Matchers.is("image/*"));
+        MatcherAssert.assertThat(MediaType.IMAGE_GIF.toString(), Matchers.is("image/gif"));
+        MatcherAssert.assertThat(MediaType.IMAGE_JPEG.toString(), Matchers.is("image/jpeg"));
+        MatcherAssert.assertThat(MediaType.IMAGE_PNG.toString(), Matchers.is("image/png"));
 
-        assertThat(MediaType.TEXT.toString(), is("text/*"));
-        assertThat(MediaType.TEXT_HTML.toString(), is("text/html"));
-        assertThat(MediaType.TEXT_MARKDOWN.toString(), is("text/markdown"));
-        assertThat(MediaType.TEXT_PLAIN.toString(), is("text/plain"));
-        assertThat(MediaType.TEXT_XML.toString(), is("text/xml"));
+        MatcherAssert.assertThat(MediaType.TEXT.toString(), Matchers.is("text/*"));
+        MatcherAssert.assertThat(MediaType.TEXT_HTML.toString(), Matchers.is("text/html"));
+        MatcherAssert.assertThat(MediaType.TEXT_MARKDOWN.toString(), Matchers.is("text/markdown"));
+        MatcherAssert.assertThat(MediaType.TEXT_PLAIN.toString(), Matchers.is("text/plain"));
+        MatcherAssert.assertThat(MediaType.TEXT_XML.toString(), Matchers.is("text/xml"));
     }
 
     @Test
     void testCompatibility() {
-        assertThat(MediaType.ANY.isCompatible(MediaType.ANY), is(true));
-        assertThat(MediaType.ANY.isCompatible(MediaType.APPLICATION), is(true));
-        assertThat(MediaType.ANY.isCompatible(MediaType.APPLICATION_JSON), is(true));
-        assertThat(MediaType.ANY.isCompatible(MediaType.APPLICATION_JSON_UTF8), is(true));
-        assertThat(MediaType.ANY.isCompatible(MediaType.APPLICATION_ANY_JSON), is(true));
+        MatcherAssert.assertThat(MediaType.ANY.isCompatible(MediaType.ANY), Matchers.is(true));
+        MatcherAssert.assertThat(MediaType.ANY.isCompatible(MediaType.APPLICATION), Matchers.is(true));
+        MatcherAssert.assertThat(MediaType.ANY.isCompatible(MediaType.APPLICATION_JSON), Matchers.is(true));
+        MatcherAssert.assertThat(MediaType.ANY.isCompatible(MediaType.APPLICATION_JSON_UTF8), Matchers.is(true));
+        MatcherAssert.assertThat(MediaType.ANY.isCompatible(MediaType.APPLICATION_ANY_JSON), Matchers.is(true));
 
-        assertThat(MediaType.APPLICATION.isCompatible(MediaType.ANY), is(false));
-        assertThat(MediaType.APPLICATION.isCompatible(MediaType.APPLICATION), is(true));
-        assertThat(MediaType.APPLICATION.isCompatible(MediaType.APPLICATION_JSON), is(true));
-        assertThat(MediaType.APPLICATION.isCompatible(MediaType.APPLICATION_JSON_UTF8), is(true));
-        assertThat(MediaType.APPLICATION.isCompatible(MediaType.APPLICATION_ANY_JSON), is(true));
-        assertThat(MediaType.APPLICATION.isCompatible(MediaType.TEXT), is(false));
+        MatcherAssert.assertThat(MediaType.APPLICATION.isCompatible(MediaType.ANY), Matchers.is(false));
+        MatcherAssert.assertThat(MediaType.APPLICATION.isCompatible(MediaType.APPLICATION), Matchers.is(true));
+        MatcherAssert.assertThat(MediaType.APPLICATION.isCompatible(MediaType.APPLICATION_JSON), Matchers.is(true));
+        MatcherAssert.assertThat(
+            MediaType.APPLICATION.isCompatible(MediaType.APPLICATION_JSON_UTF8),
+            Matchers.is(true)
+        );
+        MatcherAssert.assertThat(MediaType.APPLICATION.isCompatible(MediaType.APPLICATION_ANY_JSON), Matchers.is(true));
+        MatcherAssert.assertThat(MediaType.APPLICATION.isCompatible(MediaType.TEXT), Matchers.is(false));
 
-        assertThat(MediaType.APPLICATION_JSON.isCompatible(MediaType.APPLICATION_JSON), is(true));
-        assertThat(MediaType.APPLICATION_JSON_UTF8.isCompatible(MediaType.APPLICATION_JSON_UTF8), is(true));
-        assertThat(MediaType.APPLICATION_ANY_JSON.isCompatible(MediaType.APPLICATION_ANY_JSON), is(true));
+        MatcherAssert.assertThat(
+            MediaType.APPLICATION_JSON.isCompatible(MediaType.APPLICATION_JSON),
+            Matchers.is(true)
+        );
+        MatcherAssert.assertThat(
+            MediaType.APPLICATION_JSON_UTF8.isCompatible(MediaType.APPLICATION_JSON_UTF8),
+            Matchers.is(true)
+        );
+        MatcherAssert.assertThat(
+            MediaType.APPLICATION_ANY_JSON.isCompatible(MediaType.APPLICATION_ANY_JSON),
+            Matchers.is(true)
+        );
 
-        assertThat(MediaType.APPLICATION_JSON.isCompatible(MediaType.APPLICATION_JSON_UTF8), is(true));
-        assertThat(MediaType.APPLICATION_JSON_UTF8.isCompatible(MediaType.APPLICATION_JSON), is(true));
+        MatcherAssert.assertThat(
+            MediaType.APPLICATION_JSON.isCompatible(MediaType.APPLICATION_JSON_UTF8),
+            Matchers.is(true)
+        );
+        MatcherAssert.assertThat(
+            MediaType.APPLICATION_JSON_UTF8.isCompatible(MediaType.APPLICATION_JSON),
+            Matchers.is(true)
+        );
 
-        assertThat(MediaType.APPLICATION_ANY_JSON.isCompatible(MediaType.APPLICATION_JSON), is(false));
-        assertThat(MediaType.APPLICATION_ANY_JSON.isCompatible(MediaType.APPLICATION_JSON_UTF8), is(false));
-        assertThat(MediaType.APPLICATION_JSON.isCompatible(MediaType.APPLICATION_JSON_UTF8), is(true));
-        assertThat(MediaType.APPLICATION_JSON_UTF8.isCompatible(MediaType.APPLICATION_JSON_UTF8), is(true));
+        MatcherAssert.assertThat(
+            MediaType.APPLICATION_ANY_JSON.isCompatible(MediaType.APPLICATION_JSON),
+            Matchers.is(false)
+        );
+        MatcherAssert.assertThat(
+            MediaType.APPLICATION_ANY_JSON.isCompatible(MediaType.APPLICATION_JSON_UTF8),
+            Matchers.is(false)
+        );
+        MatcherAssert.assertThat(
+            MediaType.APPLICATION_JSON.isCompatible(MediaType.APPLICATION_JSON_UTF8),
+            Matchers.is(true)
+        );
+        MatcherAssert.assertThat(
+            MediaType.APPLICATION_JSON_UTF8.isCompatible(MediaType.APPLICATION_JSON_UTF8),
+            Matchers.is(true)
+        );
     }
 }
