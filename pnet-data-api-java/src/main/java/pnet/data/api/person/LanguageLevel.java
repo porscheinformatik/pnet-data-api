fa3fe8ff65a1d024a5546a97d58c00ef9733bd66
Carolin Schuntermann <carolin.schuntermann@porscheinformatik.com>
2026-05-21 08:47:22 +0200

    Deprecate `LanguageLevel`
    The field was never populated with any data and is effectively dead.
    
    Done for PNET-7478

--- pnet-data-api-java/src/main/java/pnet/data/api/person/LanguageLevel.java
+++ pnet-data-api-java/src/main/java/pnet/data/api/person/LanguageLevel.java
@@ -4,21 +4,31 @@ package pnet.data.api.person;
  * The competence levels according to the common European reference levels.
  *
  * @author HAM
+ * @deprecated since 3.2.5
  */
+@Deprecated(since = "3.2.5", forRemoval = true)
 public enum LanguageLevel {
+    @Deprecated(since = "3.2.5", forRemoval = true)
     UNDEFINED,
 
+    @Deprecated(since = "3.2.5", forRemoval = true)
     A1,
+    @Deprecated(since = "3.2.5", forRemoval = true)
     A2,
 
+    @Deprecated(since = "3.2.5", forRemoval = true)
     B1,
+    @Deprecated(since = "3.2.5", forRemoval = true)
     B2,
 
+    @Deprecated(since = "3.2.5", forRemoval = true)
     C1,
+    @Deprecated(since = "3.2.5", forRemoval = true)
     C2,
 
     /**
      * Used for deserialization of unknown enum values from JSON (see UnknownEnumDeserializationHandler).
      */
+    @Deprecated(since = "3.2.5", forRemoval = true)
     UNKNOWN,
 }
