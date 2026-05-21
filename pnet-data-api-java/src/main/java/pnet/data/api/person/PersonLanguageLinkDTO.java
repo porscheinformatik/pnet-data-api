c15aecf87b8ba4832b3f3e5378e48cdd60b00ca8
Carolin Schuntermann <carolin.schuntermann@porscheinformatik.com>
2026-05-21 08:47:22 +0200

    Deprecate `LanguageLevel`
    The field was never populated with any data and is effectively dead.
    
    Done for PNET-7478
    
    (cherry picked from commit fa3fe8ff65a1d024a5546a97d58c00ef9733bd66)

--- pnet-data-api-java/src/main/java/pnet/data/api/person/PersonLanguageLinkDTO.java
+++ pnet-data-api-java/src/main/java/pnet/data/api/person/PersonLanguageLinkDTO.java
@@ -19,12 +19,13 @@ public class PersonLanguageLinkDTO implements Serializable {
     @Schema(description = "The language as IETF language tag.")
     private final Locale language;
 
-    @Schema(description = "The level of competence.")
+    @Schema(description = "The level of competence. Deprecated since 3.2.5, as it was never populated with any data.")
+    @Deprecated(since = "3.2.5", forRemoval = true)
     private final LanguageLevel level;
 
     public PersonLanguageLinkDTO(
-        @JsonProperty("language") Locale language,
-        @JsonProperty("level") LanguageLevel level
+        @JsonProperty("language") Locale language,        
+        @Deprecated(since = "3.2.5", forRemoval = true) @JsonProperty("level") LanguageLevel level
     ) {
         super();
         this.language = language;
@@ -35,13 +36,14 @@ public class PersonLanguageLinkDTO implements Serializable {
         return language;
     }
 
+    @Deprecated(since = "3.2.5", forRemoval = true)
     public LanguageLevel getLevel() {
         return level;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(language, level);
+        return Objects.hash(language);
     }
 
     @Override
@@ -55,12 +57,6 @@ public class PersonLanguageLinkDTO implements Serializable {
         if (!(obj instanceof PersonLanguageLinkDTO other)) {
             return false;
         }
-        if (!Objects.equals(language, other.language)) {
-            return false;
-        }
-        if (level != other.level) {
-            return false;
-        }
-        return true;
+        return Objects.equals(language, other.language);
     }
 }
