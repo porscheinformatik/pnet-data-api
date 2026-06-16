c600dd35532d190f86f21f048d368a9da54d2939
Benjamin Wieser <benjamin.wieser@vorstieg.eu>
2026-06-16 10:13:00 +0200

    PNET-7796 introduce optional rectifier

--- pnet-data-api-java/src/main/java/pnet/data/api/person/PersonHierarchyType.java
+++ pnet-data-api-java/src/main/java/pnet/data/api/person/PersonHierarchyType.java
@@ -3,6 +3,7 @@ package pnet.data.api.person;
 public enum PersonHierarchyType {
     RESPONSIBLE,
     RECERTIFIER,
+    OPTIONAL_RECERTIFIER,
     /**
      * Used for deserialization of unknown enum values from JSON (see UnknownEnumDeserializationHandler).
      */
