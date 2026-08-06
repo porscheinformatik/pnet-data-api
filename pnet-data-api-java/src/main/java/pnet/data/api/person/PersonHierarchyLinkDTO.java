94aceb8d7ff3f693e6385bad2f29802b95182178
Carolin Schuntermann <carolin.schuntermann@porscheinformatik.com>
2026-08-06 17:32:17 +0200

    Correct copyright creation dates
    It was either this or always updating the copyright to the current year,
    and the latter has too much upkeep.

--- pnet-data-api-java/src/main/java/pnet/data/api/person/PersonHierarchyLinkDTO.java
+++ pnet-data-api-java/src/main/java/pnet/data/api/person/PersonHierarchyLinkDTO.java
@@ -1,4 +1,4 @@
-/* Copyright 2017 Porsche Informatik GmbH
+/* Copyright 2024 Porsche Informatik GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
